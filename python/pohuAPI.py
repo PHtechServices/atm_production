95b3f43f0f6a3d81ebf096bc8dbf8cf7ac3631b3
Himanshu Singh1 <himanshu_singh1@epam.com>
2021-10-07 14:56:14 +0530

    Port Number Changed

--- python/pohuAPI.py
+++ python/pohuAPI.py
@@ -129,4 +129,4 @@ def delete_collection():
     temp = deletecollection(req_data)
     return jsonify({"message":"Collection Deleted"})
 
-app.run(debug=True, port=3000, host="0.0.0.0")
+app.run(debug=True, port=5000, host="0.0.0.0")
